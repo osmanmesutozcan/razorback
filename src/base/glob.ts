e3571d0a06c7b435b70b5a0fbecaecd1b5c0e347
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-02-01 16:17:39 +0300

    add new todo

--- src/base/glob.ts
+++ src/base/glob.ts
@@ -14,9 +14,12 @@ export function parseSync(_pattern: string | rback.RelativePattern) {
   // TODO: respect gitignore
   const paths = glob.sync(pattern);
 
-  logger.debug('AAAAAAAAAAlll', paths);
-
   const cache = new LRU<string, boolean>(20);
+  // TODO: This function should check if path can be globbed by the pattern.
+  // Precompiling the glob pattern is a wrong approach because
+  // file watcher actually catches new added files but because
+  // glob paths does not include them, it fails to dispatch
+  // events about new added files.
   return function (_path: string): boolean {
     const cached = cache.get(_path);
     if (cached) {
